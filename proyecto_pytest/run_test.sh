b65fff2f3b00c444517fe6062a2617954c5c833b
isaMirka <quispesicham@gmail.com>
2026-02-11 22:00:19 -0500

    finalizando cambios

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -2,16 +2,25 @@
 
 echo "inicializando ejecucion"
 
-python3 -m venv venv
-source venv/bin/activate
+if [ ! -d "venv"]; then
+    echo "entono virtual no creado"
+    python3 -m venv venv
+fi
+
+if [ -f venv/bin/activate ]; then
+    source venv/bin/activate
+elif [ -f venv/scripts/activate ]; then
+    source venv/scripts/activate
+else
+    echo "no se pudo activar el entorno"
+    exit 1
+fi
 
 echo "instalando dependencias"
-pip install --upgrade pip
-pip install pytest pytest-html
-
-mkdir -p reports
+pip install --upgrade pip --break-system-packages
+pip install -r requirement.txt --break-system-packages
 
 echo "ejecutando las pruebas con pytest"
-pytest test/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+venv/bin/python -m pytest test/ test/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
 
 echo "pruebas finalizadas, resultados en reports"
