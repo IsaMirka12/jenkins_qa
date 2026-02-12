7b87238bed505ba3c76b7cdd8f4aafdbd432a597
isaMirka <quispesicham@gmail.com>
2026-02-12 12:26:49 -0500

    agregando los cambios del sh

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -1,21 +1,20 @@
-#!/bin/bash
-set -e  # detiene el script si algo falla
+#!/bin/sh
+set -e
 
 echo "inicializando ejecucion"
+pwd
+whoami
 
-# Verifica si existe el entorno virtual
 if [ ! -d "venv" ]; then
-    echo "entorno virtual no creado"
+    echo "creando entorno virtual"
     python3 -m venv venv
 fi
 
-# Activar entorno
 if [ -f "venv/bin/activate" ]; then
-    source venv/bin/activate
-elif [ -f "venv/Scripts/activate" ]; then
-    source venv/Scripts/activate
+    . venv/bin/activate
 else
     echo "no se pudo activar el entorno"
+    ls -la
     exit 1
 fi
 
@@ -23,7 +22,7 @@ echo "instalando dependencias"
 pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "ejecutando las pruebas con pytest"
+echo "ejecutando pytest"
 python -m pytest test/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
 
-echo "pruebas finalizadas, resultados en reports"
+echo "pruebas finalizadas"
