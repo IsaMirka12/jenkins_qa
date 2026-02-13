59abfa6a4104091b662b82c97c17435c6e323aec
isaMirka <quispesicham@gmail.com>
2026-02-12 22:28:54 -0500

    primera ejecucion

---
+++ saludo.py
@@ -0,0 +1,7 @@
+print("archivo ejecutado en python")
+print("hola mundo desde python")
+
+print("hola mundo desde python")
+print("hola mundo desde python")
+print("hola mundo desde python")
+print("hola mundo desde python")
